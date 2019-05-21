1113b9f693bb00ce34ac64f019168d6526e0a799
miao <miao@zhaomiaomiao-3.local>
2019-05-21 17:38:34 +0800

    Create ZMMADBWeiXinShareModules.m
    新增微信分享的Modules

---
+++ ZMMADModuleCenter/ZMMADModuleCenter/ADModules/Modules/ZMMADBWeiXinShareModules.m
@@ -0,0 +1,30 @@
+//
+//  ZMMADBWeiXinShareModules.m
+//  ZMMADModuleCenter
+//
+//  Created by miao on 2019/5/21.
+//  Copyright © 2019年 miao. All rights reserved.
+//
+
+#import "ZMMADBWeiXinShareModules.h"
+#import "AppDelegate.h"
+
+@implementation ZMMADBWeiXinShareModules
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+    //向微信注册 add by zgp
+   
+    
+    return YES;
+}
+
+
+- (BOOL)application:(UIApplication *)app openURL:(NSURL *)url options:(NSDictionary<NSString*, id> *)options
+{
+   
+    return YES;
+}
+
+
+@end
