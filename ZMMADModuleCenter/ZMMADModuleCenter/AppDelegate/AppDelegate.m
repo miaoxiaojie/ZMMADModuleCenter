920e3d91c5ae754a127a8b987a95fef13664c2f4
miao <miao@zhaomiaomiao-3.local>
2019-05-21 17:36:37 +0800

    Update AppDelegate.m
    新增微信分享的Modules

--- ZMMADModuleCenter/ZMMADModuleCenter/AppDelegate/AppDelegate.m
+++ ZMMADModuleCenter/ZMMADModuleCenter/AppDelegate/AppDelegate.m
@@ -92,6 +92,20 @@
     }];
 }
 
+- (BOOL)application:(UIApplication *)app openURL:(NSURL *)url options:(NSDictionary<NSString*, id> *)options
+{
+    
+    __block BOOL result = NO;
+    ZMMADStartApplicationCenter *center = [ZMMADStartApplicationCenter sharedAppEventCenter];
+    [center handleApplicationEvent:@"applicationDidEnterBackground:" Complete:^(id  _Nonnull module, SEL  _Nonnull sel) {
+        
+        IMP imp = [module methodForSelector:sel];
+        void (*func)(id, SEL,UIApplication *,NSURL *,NSDictionary<NSString*, id> *) = (void *)imp;
+        func(module,sel,app,url,options);
+        result = YES;
+    }];
+    return result;
+}
 
 - (void)applicationWillTerminate:(UIApplication *)application {
     ZMMADStartApplicationCenter *center = [ZMMADStartApplicationCenter sharedAppEventCenter];
