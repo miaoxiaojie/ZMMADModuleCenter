32f355b968ade86a7775e5bf54c42ac5ffb33572
miao <miao@zhaomiaomiao-3.local>
2019-05-21 17:38:28 +0800

    Create ZMMADBWeiXinShareModules.h
    新增微信分享的Modules

---
+++ ZMMADModuleCenter/ZMMADModuleCenter/ADModules/Modules/ZMMADBWeiXinShareModules.h
@@ -0,0 +1,17 @@
+//
+//  ZMMADBWeiXinShareModules.h
+//  ZMMADModuleCenter
+//
+//  Created by miao on 2019/5/21.
+//  Copyright © 2019年 miao. All rights reserved.
+//
+
+#import "ZMMADStartBaseModule.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ZMMADBWeiXinShareModules : ZMMADStartBaseModule
+
+@end
+
+NS_ASSUME_NONNULL_END
